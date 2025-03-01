9c88272fc2a90d48dcb7e4ed85c118438bce51be
nisargb <59101043+nisargb@users.noreply.github.com>
2025-02-28 23:02:21 -0500

    Update StreamExample.java

--- StreamExample.java
+++ StreamExample.java
@@ -27,33 +27,33 @@ public class StreamExample {
 
         /// Operations
         List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-//
-//        //Filter
-//        List<Integer> resultList = numberList.stream()
-//                .peek(System.out :: println) // allows you to see the current operated value
-//                .filter(x -> x % 2 == 0)
-//                .collect(Collectors.toList());  // filter
-//        System.out.println(resultList);
 
-        //Map
-//        List<Integer> mappedList = numberList.stream()
-//                .map(x -> x * 5)
-//                .collect(Collectors.toList());
-//        System.out.println(mappedList);
+       //Filter
+       List<Integer> resultList = numberList.stream()
+               .peek(System.out :: println) // allows you to see the current operated value
+               .filter(x -> x % 2 == 0)
+               .collect(Collectors.toList());  // filter
+       System.out.println(resultList);
 
-//        // both filter and map together
-//        List<Integer> filteredMappedList = numberList.stream()
-//                .filter(x -> x % 2 == 0)
-//                .peek(System.out :: print)
-//                .map(x -> x * 5)
-//                .collect(Collectors.toList());
-//        System.out.println(filteredMappedList);
+        Map
+       List<Integer> mappedList = numberList.stream()
+               .map(x -> x * 5)
+               .collect(Collectors.toList());
+       System.out.println(mappedList);
+
+       // both filter and map together
+       List<Integer> filteredMappedList = numberList.stream()
+               .filter(x -> x % 2 == 0)
+               .peek(System.out :: print)
+               .map(x -> x * 5)
+               .collect(Collectors.toList());
+       System.out.println(filteredMappedList);
 //
 //        // more operations
            numberList = Arrays.asList(100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
           List<Integer> operatedList = numberList.stream()
                 .filter(x -> x % 2 == 0)
-//                  .peek(System.out :: print)
+                  .peek(System.out :: print)
                   .map(x -> x * 5)
                 .distinct()
                 .sorted()
